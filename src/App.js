a89a6d9fe84f292bdc19d69698ac326f9bf7f907
LL Powe <powe2009@gmail.com>
2022-01-21 14:29:10 -0500

    React GraphQL useMutation Update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,139 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  useQuery,
+  useMutation,
+  gql
+} from "@apollo/client";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+  const client = new ApolloClient({
+    uri: "https://sxewr.sse.codesandbox.io/",
+    cache: new InMemoryCache()
+  });
+
+  const GET_TODOS = gql`
+    {
+      todos {
+        id
+        type
+      }
+    }
+  `;
+
+const UPDATE_TODO = gql`
+  mutation ($id: String!, $type: String!) {
+    updateTodo(id: $id, type: $type) {
+      id
+      type
+    }
+  }
+  `;
+
+  const Todos = () => {
+    const {loading, error, data } = useQuery(GET_TODOS);
+    const [todos, setTodos] = React.useState([]);
+    const [input, setInput] = React.useState('');
+    const [message, setMessage] = React.useState('')
+
+    const [
+      updateTodo,
+      { loading: mutLoading, error: mutError }
+    ] = useMutation(UPDATE_TODO);
+
+    React.useEffect( () => {
+      setTodos(data?.todos)
+    }, [data])
+ 
+    if (loading)  return <p>Loading...</p> 
+    if (error) return <p>Error :(</p>;
+    if (!todos) return <p></p>
+
+    const handleChange = (e, i) => {
+      setInput(e.target.value)
+      // console.log('input', input)
+    }
+  
+    console.log('todos', todos)
+
+    const handleSubmit = (e, id, type) => {
+      e.preventDefault()
+      // console.log('handleSubmit', id, type, input)
+      updateTodo({ variables: { id, type: input } });
+      setMessage(`Todo type updated to ${input}`)
+      setInput('')
+    }
+
+    return (
+      <>
+        <form
+          onReset={e => {
+            setInput('')
+            setMessage('')
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <table>
+              <thead>
+                <tr>
+                  <th>Current Value</th>
+                  <th>New Value</th>      
+                  <th>Changing Value</th>
+                  <th></th>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody>
+              {
+                todos.map( ({id, type}) => (
+                  <tr key={id}>
+                    <td>{type}</td>
+                    <td>
+                      <input id='type' name='type' 
+                        onChange={handleChange}
+                      />
+                    </td>
+                    <td>
+                      {input}
+                    </td>
+                    <td>
+                      <button type='submit' className='updB' onClick={(e) => handleSubmit(e, id, type)}>Update</button>
+                    </td>
+                    <td>
+                    <button type='reset' className='resetB'>Reset</button>
+                    </td>
+                  </tr>
+                ))
+              }
+              </tbody>
+              <tfoot>
+                <tr>
+                  <td colspan='5'>
+                    <b>Messages: </b>
+                    {message}
+            
+                  </td>
+                </tr>
+              </tfoot>
+          </table>
+        </form>
+
+      </>
+    )
+  }
+
+  return (
+    <ApolloProvider client={client}>
+      <div className="App">
+        <h3>React GraphQL / useMutation / Update</h3>
+
+        <Todos />
+
+      </div>
+    </ApolloProvider>
+
   );
 }
 
