57f1ce1f8e87d8760a7ccdd5dcd82a2377a81a62
LL Powe <powe2009@gmail.com>
2022-01-27 12:41:04 -0500

    refactored components

--- src/App.js
+++ src/App.js
@@ -9,20 +9,15 @@ import {
   gql
 } from "@apollo/client";
 
-function App() {
-  const client = new ApolloClient({
-    uri: "https://sxewr.sse.codesandbox.io/",
-    cache: new InMemoryCache()
-  });
 
-  const GET_TODOS = gql`
-    {
-      todos {
-        id
-        type
-      }
-    }
-  `;
+const GET_TODOS = gql`
+{
+  todos {
+    id
+    type
+  }
+}
+`;
 
 const UPDATE_TODO = gql`
   mutation ($id: String!, $type: String!) {
@@ -31,106 +26,95 @@ const UPDATE_TODO = gql`
       type
     }
   }
-  `;
+`;
 
-  const Todos = () => {
-    const {loading, error, data } = useQuery(GET_TODOS);
-    const [todos, setTodos] = React.useState([]);
-    const [input, setInput] = React.useState('');
-    const [message, setMessage] = React.useState('')
-
-    const [
-      updateTodo,
-      { loading: mutLoading, error: mutError }
-    ] = useMutation(UPDATE_TODO);
-
-    React.useEffect( () => {
-      setTodos(data?.todos)
-    }, [data])
- 
-    if (loading)  return <p>Loading...</p> 
-    if (error) return <p>Error :(</p>;
-    if (!todos) return <p></p>
-
-    const handleChange = (e, i) => {
-      setInput(e.target.value)
-      // console.log('input', input)
-    }
-  
-    console.log('todos', todos)
-
-    const handleSubmit = (e, id, type) => {
-      e.preventDefault()
-      // console.log('handleSubmit', id, type, input)
-      updateTodo({ variables: { id, type: input } });
-      setMessage(`Todo type updated to ${input}`)
-      setInput('')
-    }
-
-    return (
-      <>
-        <form
-          onReset={e => {
-            setInput('')
-            setMessage('')
-          }}
-        >
-          <table>
-              <thead>
-                <tr>
-                  <th>Current Value</th>
-                  <th>New Value</th>      
-                  <th></th>
-                  <th></th>
-                </tr>
-              </thead>
-              <tbody>
-              {
-                todos.map( ({id, type}) => (
-                  <tr key={id}>
-                    <td>{type}</td>
-                    <td>
-                      <input id='type' name='type' 
-                        onChange={handleChange}
-                      />
-                    </td>
-                    <td>
-                      <button type='submit' className='updB' onClick={(e) => handleSubmit(e, id, type)}>Update</button>
-                    </td>
-                    <td>
-                    <button type='reset' className='resetB'>Reset</button>
-                    </td>
-                  </tr>
-                ))
-              }
-              </tbody>
-              <tfoot>
-                <tr>
-                  <td colspan='4'>
-                    <b>Messages: </b>
-                    {message}
-            
-                  </td>
-                </tr>
-              </tfoot>
-          </table>
-        </form>
-
-      </>
-    )
-  }
+function App() {
+  const client = new ApolloClient({
+    uri: "https://sxewr.sse.codesandbox.io/",
+    cache: new InMemoryCache()
+  });
 
   return (
     <ApolloProvider client={client}>
       <div className="App">
-        <h3>React GraphQL / useMutation / Update</h3>
-
-        <Todos />
+        <h4>App - wrap downstream components inside ApolloProvider</h4>
+        <Main />
 
       </div>
     </ApolloProvider>
 
   );
+
+}
+
+const Main = () => {
+  const {loading, error, data } = useQuery(GET_TODOS);
+  const [todos, setTodos] = React.useState([]);
+  const [todoType, setTodoType] = React.useState('')
+
+  const [
+    updateTodo,
+    { loading: mutLoading, error: mutError }
+  ] = useMutation(UPDATE_TODO);
+
+  React.useEffect( () => {
+    setTodos(data?.todos)
+  }, [data])
+
+  if (loading)  return <p>Loading...</p> 
+  if (error) return <p>Error :(</p>;
+  if (!todos) return <p></p>
+
+  const handleChange = (e, id, type) => {
+    // console.log('handleChange', id, type, e.target.value)
+    setTodoType(e.target.value)
+  }
+
+  const handleUpdate = (e, id, type) => {
+    // console.log('handleUpdate',  id, todoType)
+
+    updateTodo({ variables: { id, type: todoType } });
+    // console.log('afterUpdateTodo', mutError, mutLoading)
+  }
+
+  return (
+    <>
+      <h4>Main - maintain state </h4>
+      <Todos 
+        todos={todos} onHandleChange={handleChange} onHandleUpdate={handleUpdate}
+      />
+    </>
+  )
+}
+
+const Todos = ({todos, onHandleChange, onHandleUpdate}) => {
+  // console.log('todos', todos)
+
+  return (
+    <>
+      <h5>Todos - handle input</h5>
+        <table>
+        <tbody>
+        {
+          todos.map( ({id, type}) => (
+            <tr key={id}>
+              <td>{type} </td>
+              <td>
+              <input id='type' name='type' 
+                  onChange={(e) => onHandleChange(e, id, type)}
+              />
+              </td>
+
+              <td>
+              <button className='updB' onClick={(e) => onHandleUpdate(e, id, type)}>Update</button>
+              </td>
+            </tr>
+          ))
+        }
+        </tbody>
+        </table>
+    </>
+  )
 }
 
 export default App;
